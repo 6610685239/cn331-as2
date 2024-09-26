547a842cac581903eba0cc7cc6c357c3e11aa1cb
Parunchai Timklip <6610685239@cn103>
2024-09-27 03:03:39 +0700

    complete

--- WebApp/quota_manage/urls.py
+++ WebApp/quota_manage/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import quota_list, cancel_quota
 
 urlpatterns = [
-    path('', quota_list, name='quota_list'),
-    path('cancel/<int:enrollment_id>/', cancel_quota, name='cancel_quota'),
+    path("quota-manage", quota_list, name="quota_list"),
+    path("cancel/<int:enrollment_id>/", cancel_quota, name="cancel_quota"),
 ]
--- WebApp/quota_manage/views.py
+++ WebApp/quota_manage/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from enrollment.models import Enrollment
 from courses.models import Course
+from django.contrib import messages
 
 # Create your views here.
 
+
 @login_required
 def quota_list(request):
+    user = request.user
     enrollments = Enrollment.objects.filter(user=request.user)
 
-    return render(request, 'quota_manage/quota_list.html', {'enrollments' : enrollments})
+    return render(request, "quota_manage/quota_list.html", {"enrollments": enrollments})
+
 
 @login_required
 def cancel_quota(request, enrollment_id):
@@ -17,7 +21,7 @@ def cancel_quota(request, enrollment_id):
     course = enrollment.course
 
     enrollment.delete()
-
-    course.enrolled_user.remove(request.user)
-
-    return redirect('quota_list')
+    course.enrolled_users.remove(request.user)
+    course.save()
+    messages.success(request, "ยกเลิกการลงทะเบียนสำเร็จ")
+    return redirect("quota_list")
